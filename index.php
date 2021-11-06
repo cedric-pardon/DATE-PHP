b77295a87290300bb1cd0e98fa77ce92ef98e3cd
cédric <83868499+cedric-pardon@users.noreply.github.com>
2021-11-06 17:57:48 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+$presentTime = new DateTime('now');
+$destinationTime = new DateTime('10/21/2015 04:06');
+
+// var_dump($presentTime);
+// var_dump($destinationTime);
+
+$interval = $presentTime->diff($destinationTime);
+
+//var_dump($interval);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="destination-time"> <?= 'Destination time is ' . $destinationTime->format('M d Y h:i:s') . '<br>'; ?></div>
+    <div class="present-time"> <?= 'Present time is ' . $presentTime->format('M d Y h:i:s') . '<br>'; ?></div>
+    <div class="time-bridge"> <?= 'The time of the time bridge' . $interval->format(" %Y years %M months %D days %H hours %I minutes %S seconds") . '<br>'; ?></div>
+
+</body>
+
+</html>
